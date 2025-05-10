5276b1bc8b63cfc77ecc51cb01fd535123121d96
nara-1-simha <narasimhareddykoduri123@gmail.com>
2025-05-10 23:11:05 +0530

    Initial commit for ecommerce project

---
+++ angular-11-main/MainProject/ecommerce/src/app/prasad.directive.spec.ts
@@ -0,0 +1,9 @@
+
+import { PrasadDirective } from './prasad.directive';
+
+describe('PrasadDirective', () => {
+  it('should create an instance', () => {
+    const directive = new PrasadDirective();
+    expect(directive).toBeTruthy();
+  });
+});
